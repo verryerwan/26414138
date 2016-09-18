7aa928daa201072f6fff7b7b7d551a931293f181
verryerwan <m26414138@john.petra.ac.id>
2016-09-18 23:33:54 +0700

    ode 5

---
+++ code5.sh
@@ -0,0 +1,17 @@
+ for ((i=a;i>0;i--))
+        {
+                for ((j=0;j<i;j++))
+                {
+                        echo -n "*";
+                }
+                echo "";
+        }
+         for ((i=1;i<=a;i++))
+        {
+                for ((j=1;j<=i;j++))
+                {
+                        echo -n "*";
+                }
+                echo "";
+        }
+

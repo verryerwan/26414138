34e7ed69cd5f6d19bd24c64186c8f983175d4cc0
verryerwan <m26414138@john.petra.ac.id>
2016-09-18 23:27:25 +0700

    code 3

---
+++ code3.sh
@@ -0,0 +1,18 @@
+ for ((i=1;i<=a;i++))
+        {
+                for ((j=1;j<=i;j++))
+                {
+                        echo -n "*";
+                }
+                echo "";
+        }
+        for ((i=a;i>1;i--))
+        {
+                for ((j=1;j<i;j++))
+                {
+                        echo -n "*";
+                }
+                echo "";
+        }
+
+

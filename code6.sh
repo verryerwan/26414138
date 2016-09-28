51c5d8eb84738c2c497afece87642783f9a5edba
verryerwan <m26414138@john.petra.ac.id>
2016-09-29 06:49:33 +0700

    code 6

--- code6.sh
+++ code6.sh
@@ -1,1 +1,17 @@
-a
+let i;
+let n=0;
+let a;
+read i;
+until ((n >= i * 2 + 1))
+do
+        for ((a=0;a<=i * 2 + 2;a++))
+                {
+                        if (($n % 2 == 0))
+                        then
+                                if (($a % 2 == 0))
+                                then
+                                        echo -n "+";
+                                else
+                                        echo -n "-";
+                                fi
+
